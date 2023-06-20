e2cac628473ae5f15441a8b4b8c2bd7b4d2b3f06
adinrama <adinrama@student.ub.ac.id>
2023-06-20 11:05:55 +0700

    Menangani CORS

--- app.js
+++ app.js
@@ -24,12 +24,12 @@ const practicesPointsRouter = require("./routes/practice_points");
 
 const app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/api/users", usersRouter);
